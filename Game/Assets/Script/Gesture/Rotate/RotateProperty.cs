b6868bf49875a666b4b4fd85e007cd1aa4b13f23
al1c3x <80011970+al1c3x@users.noreply.github.com>
2023-08-07 22:18:02 +0800

    //

---
+++ Game/Assets/Script/Gesture/Rotate/RotateProperty.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RotateProperty 
+{
+    public float MinDistance = 0.4f;
+    public float minAngleChange = 4f;
+}
